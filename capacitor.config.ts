80a13ed3a96568d05ac9a3d40bc8d9ed85fef60d
Marcos Gomes <marcosartemio221@gmail.com>
2024-09-11 17:26:54 -0300

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'krm.client',
   appName: 'krm-client',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {          //
+    CapacitorHttp: {  //
+      enabled: true,  // Remover em PROD
+    },                //
+  },                  //
 };
 
 export default config;
